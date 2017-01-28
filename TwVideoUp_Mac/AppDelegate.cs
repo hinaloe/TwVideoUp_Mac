a1fbe9518d5286a9739d8b2328e1cef7434792fa
hina <hina@hinaloe.net>
2017-01-28 23:57:29 +0900

    窓を閉じたら終了するように

--- TwVideoUp_Mac/AppDelegate.cs
+++ TwVideoUp_Mac/AppDelegate.cs
@@ -3,21 +3,26 @@ using Foundation;
 
 namespace TwVideoUp_Mac
 {
-	public class AppDelegate : NSApplicationDelegate
-	{
-		public AppDelegate()
-		{
-		}
+    [Register("AppDelegate")]
+    public class AppDelegate : NSApplicationDelegate
+    {
+        public AppDelegate()
+        {
+        }
 
-		public override void DidFinishLaunching(NSNotification notification)
-		{
-			// Insert code here to initialize your application
-		}
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            // Insert code here to initialize your application
+        }
 
-		public override void WillTerminate(NSNotification notification)
-		{
-			// Insert code here to tear down your application
-		}
-	}
+//        public override void WillTerminate(NSNotification notification)
+//        {
+//            // Insert code here to tear down your application
+//        }
+
+        public override bool ApplicationShouldTerminateAfterLastWindowClosed(NSApplication sender)
+        {
+            return true;
+        }
+    }
 }
-
